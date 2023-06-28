4fb18fb7a7bb0ee86327d34bc6131f231c8ee73b
R. Bryant <rkbry@outlook.com>
2023-06-28 09:20:41 -0700

    Added basic algorithm, functions are TODO.

--- kaprekar.c
+++ kaprekar.c
@@ -8,7 +8,22 @@ See https://en.wikipedia.org/wiki/6174
 #include <stdlib.h>
 #include <stdio.h>
 
+int greater(int index);
+int lesser(int index);
+
 int main(void)
 {
-    // TODO
+    const int KAP = 6174;
+
+    for (int i = 1000; i < 10000; i++) {
+        if (i % 1111 == 0) {
+            // A minimum of two different digits required.
+            continue;
+        }
+        if (greater(i) - lesser(i) == KAP) {
+            printf("%d", i);
+        }
+    }
+    
+    return 0;
 }
