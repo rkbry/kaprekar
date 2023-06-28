f9e458db880f3608dd19c629b9c24b9252ae6e8a
R. Bryant <rkbry@outlook.com>
2023-06-28 08:53:21 -0700

    Initial code-- nothing here yet.

---
+++ kaprekar.c
@@ -0,0 +1,14 @@
+/*
+Simple program to list all four-digit integers that reach Kaprekar's constant
+in a single step.
+
+See https://en.wikipedia.org/wiki/6174
+
+*/
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(void)
+{
+    // TODO
+}
